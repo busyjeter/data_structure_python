a9f9f8bd76c0b547e538638f19e8604d315dfaaf
Jeter <Jeter@jeterMBP.lan>
2018-07-21 20:15:07 +0800

    josephus_2_method

--- josephus.py
+++ josephus.py
@@ -20,7 +20,56 @@ def josephus(n,k,m):
 		num-=1#存活人数-1
 		#当最后只剩一个人的时候，循环2圈把自己数了2次 算2个人 
 		#输出最后这个人
-josephus(10,3,2)#57913610428
+
+
+
+def josephus_A(n,k,m):
+	#n是人数，k是开始报数，m是第m个人报数退出
+	people= list(range(1,n+1))
+
+	i=k-1#index
+	for num in range(n):#每次循环取出一个人
+		count=0
+		while count<m:
+			if people[i]>0:
+				count+=1
+			if count==m:
+				print(people[i],end="")
+				people[i]=0
+			i=(i+1)%n #!这个实际上是i+=1，但是经过n以后，会从头开始循环，eg是n=10
+			#是因为求模运算的特点，a%b，当a比b小时，求模运算=a ，当a比b大1，就余1，意味着
+			#i可以在0~n之间循环
+		if num<n-1:
+			print(",",end="")#
+		else:
+			print("")
+	return
+
+josephus_A(10,3,2)#57913610428
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
